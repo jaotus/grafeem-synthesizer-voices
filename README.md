ac1a236f5909694441d530ae0bfba0b7fd4fb5de
jaotus <t@pajupuu.eu>
2023-02-27 07:49:24 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,16 @@
+## `grafeem_synthesizer` trained voices
+
+This repo has two HMM voices:
+- Andreas1_v
+- Lemmes2
+
+Usage:
+```bash
+cd hmm
+mkdir -p voices/et
+unzip -d voices/et Andreas1_v.zip
+mkdir -p data/{txt,wav}
+echo "Lihtsalt nii on." >data/txt/text.txt
+./minuhaal.sh Andreas1_v
+aplay ./data/wav/Andreas1_v.wav
+```
--- hmm/minuhaal.sh
+++ hmm/minuhaal.sh
@@ -5,20 +5,17 @@ set -e
 LANGUAGE=et
 SPEAKER=minuhaal
 test "$1" && SPEAKER=$1
-INFILE=./test/txt/in_$SPEAKER.txt
-OUTFILE=./test/wav/out_$SPEAKER.wav
+INFILE=data/txt/text.txt
+OUTFILE=data/wav/$SPEAKER.wav
 
-mkdir -p -m 0777 corpus train voices test
+mkdir -p -m 0777 corpus train voices/et
 
-echo check podman >log.log
 podman -v &>>log.log && rt=podman || rt=docker
-echo selected $rt >>log.log
-echo "python ./scripts/speak.py -l $LANGUAGE -s $SPEAKER -o $OUTFILE naive $INFILE" >>log.log
 
 exec $rt run --rm -i \
    -v $(realpath voices):/Ossian/voices \
-   -v $(realpath test):/Ossian/test \
-   ghcr.io/jaotus/hmm-grafeem-train:latest bash &>>log.log <<EOF
+   -v $(realpath data):/Ossian/data \
+   ghcr.io/jaotus/hmm-grafeem-train:latest bash <<EOF
 export LANG=C.utf8
 export THEANO_FLAGS=""
 export USER=tester
@@ -26,14 +23,11 @@ export USER=tester
 ## conda activate ossian
 . /venv/bin/activate
 
-mkdir -p -m 0777 test/{wav,txt}
+mkdir -p -m 0777 data/{wav,txt}
 python ./scripts/speak.py -l $LANGUAGE -s $SPEAKER -o $OUTFILE naive $INFILE
 echo Done $OUTFILE
 
 cp $OUTFILE $OUTFILE.last
-rm ./test/wav/out_$SPEAKER.lab
-
-chmod -R 0777 test
+#rm ./data/wav/out_$SPEAKER.lab
 
 EOF
-
